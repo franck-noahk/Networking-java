917779650477ad11ccba84674caa8ac3e7625ba7
Noah F <franck.noahk@gmail.com>
2019-04-02 16:19:30 +0000

    Host now compiles to run the begining of code, but it will be changed in future.

--- inClass/Host.java
+++ inClass/Host.java
@@ -1,8 +1,15 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.*;
+
 public class Host{
     Socket client;
     
     public static void main(String[] args) {
-        Client c = new Client();
+        Host c = new Host();
         c.runclient();
     }
 
