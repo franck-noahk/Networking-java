2fd2f4c0e9d0c78fd7d172d8c732fcc04ed19090
Noah Franck <franck.noahk@gmail.com>
2019-04-24 14:08:20 -0500

    Added code from the teacher given at https://www.mkyong.com/java/how-to-get-mac-address-in-java/ it is to retrieve an IP address.

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,42 @@
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
 public class Client {
+
+    private static InetAddress ip;
+
+    public static void main(String[] args) {
+
+        //------------------------------Code from teacher --------------------------------------------------------------
+        try{
+            ip = InetAddress.getLocalHost();
+
+            System.out.println("Current IP address : " + ip.getHostAddress());
+
+            NetworkInterface network = null;
+
+            network = NetworkInterface.getByInetAddress(ip);
+
+
+            byte[] mac = network.getHardwareAddress();
+
+            System.out.print("Current MAC address : ");
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < mac.length; i++) {
+                sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
+            }
+            System.out.println(sb.toString());
+        } catch(SocketException e) {
+            e.printStackTrace();
+        } catch(UnknownHostException s) {
+            s.printStackTrace();
+        }
+
+        //-----------------------------------------end of code from teacher---------------------------------------------
+    }
+
+
 }
