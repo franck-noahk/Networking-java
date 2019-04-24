84fbdac912e59e4df90958d5aa367eeae9aba136
Noah Franck <franck.noahk@gmail.com>
2019-04-24 14:40:54 -0500

    Created the runClient Method. Takes the command line argument 1, and the string of the macAddress.

--- src/Client.java
+++ src/Client.java
@@ -1,11 +1,16 @@
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.net.UnknownHostException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.*;
 
 public class Client {
 
     private static InetAddress ip;
+    private static ObjectInputStream input;
+    private static ObjectOutputStream output;
+    private static String message;
+    private static Socket client;
+    private static String StringB;
 
     public static void main(String[] args) {
 
@@ -28,6 +33,7 @@ public class Client {
             for (int i = 0; i < mac.length; i++) {
                 sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
             }
+            StringB = sb.toString();        //My own code that should save the mac address to a string
             System.out.println(sb.toString());
         } catch(SocketException e) {
             e.printStackTrace();
@@ -36,7 +42,27 @@ public class Client {
         }
 
         //-----------------------------------------end of code from teacher---------------------------------------------
+
+        Client client = new Client();
+        client.runclient(args[1],StringB);
     }
+
+
+    private void runclient(String commandline, String macAddress){
+        try {
+            client = new Socket(commandline, 12345);
+            output = new ObjectOutputStream(client.getOutputStream());
+            input = new ObjectInputStream(client.getInputStream());
+            message = (String) input.readObject();
+            System.out.println("Recieved: " + message);
+            input.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 
 }
