a58324acf73d240bcb60d70811b5662c800abb4f
Noah Franck <franck.noahk@gmail.com>
2019-04-25 19:36:20 -0500

    Found a part of the IO exception it says 'Address alreay in use: NET_BIND'.

--- src/Server.java
+++ src/Server.java
@@ -17,34 +17,27 @@ public class Server {
     public void runServer() {
         System.out.println("Running Server on \n");
             try {
-                server = new ServerSocket(12345, 100);
-                totalNumberOfConnections += 1;
-                connection = server.accept();
-                output = new ObjectOutputStream(connection.getOutputStream());
-                input = new ObjectInputStream(connection.getInputStream());
-                //------------needs to see if connected computer is on the same network
+                while (totalNumberOfConnections < 5 && totalNumberOfErrorConnections < 3) {
+                    server = new ServerSocket(12345, 100);
+                    totalNumberOfConnections += 1;
+                    connection = server.accept();
+                    output = new ObjectOutputStream(connection.getOutputStream());
+                    input = new ObjectInputStream(connection.getInputStream());
+                    //------------needs to see if connected computer is on the same network
 
-                String message = (String) input.readObject();
-                System.out.println(message);
-                //
-                output.writeObject("welcome to my server");     //Writing to the screen and message
+                    String message = (String) input.readObject();
+                    System.out.println(message);
+                    //
+                    output.writeObject("welcome to my server");     //Writing to the screen and message
 
-                System.out.println("Number of Connections: " + totalNumberOfConnections);
-                output.flush();
-                //try {
-                //   message=(String) input.readObject();total += 1; System.out.println("Number of Connections: "+ total);
-                //} catch (ClassNotFoundException ex) {
-                //   Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
-                //}
-                // System.out.println("recieved: " + message);
-                input.close();
-                output.close();
+                    System.out.println("Number of Connections: " + totalNumberOfConnections);
+                    output.flush();
+                    output.close();
+                }
             } catch (IOException e){
-                System.out.println("IO exception");
-                totalNumberOfErrorConnections++;
+                System.out.println("IO exception\n" + e.getMessage());
             }catch(ClassNotFoundException ex) {
                 System.out.println(" class not found eception ");
-                totalNumberOfErrorConnections++;
             }
     }
 
